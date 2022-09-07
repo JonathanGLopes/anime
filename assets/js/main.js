544b0ecd0b67171b797b3181a1af7d84acb5a02d
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-09-07 01:20:46 -0300

    Adicionando funcionalidade ao botão de ver mais

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,24 @@ const $navBar = document.querySelector(".navbar");
 
 $menu.addEventListener("click", function() {
         $navBar.classList.toggle("navbar__active");
-        console.log("oi");
     });
+
+
+const $btnVer = document.querySelector(".btn-vermais");
+const $hide = document.querySelectorAll(".hide");
+
+
+/* Não é possível adicionar o EventListener com o querySelectorAll porque ele é para um elemento de cada vez.
+Já o All retorna um array com os elementos que foram selecionados e para percorrer esse array e fazer com que as classes fossem removidas e adicionas, foi utilizado o método forEach */
+$hide.forEach(function(article){
+    $btnVer.addEventListener("click", ()=>{
+        article.classList.toggle('hide');
+
+    if(article.classList.contains('hide')){
+        $btnVer.innerHTML = "Ver mais";
+    } else {
+        $btnVer.innerHTML = "Ver menos";
+    }
+    });
+
+});
